56bd8149a157cd9d0238e7d05744683fc12d7311
michaelz126 <1zhang.michael@gmail.com>
2024-11-27 14:33:09 +0000

    finished findfreeblock

--- AppointmentBook.java
+++ AppointmentBook.java
@@ -21,19 +21,20 @@ public class AppointmentBook {
 
     public int findFreeBlock(int period, int duration) {
         int block = 0;
-        for (int i = 0; i < 60; i++)
+        for (int i = 0; i < 60; i++) {
         if (isMinuteFree(period, i))
         {
             block++;
-            if (block == duration) return i - duration + i;
-            else block = 0;
-        }
-        return -1;
+            if (block == duration) return i - duration + 1;
+        } else block = 0;
     }
+    return -1;
+}
 
     public boolean makeAppointment(int startPeriod, int endPeriod, int duration){
         return false;
     }
+    
     public void printPeriod(int period) 
     {
         for(int i = 0; i < schedule[period - 1].length; i++)
--- Main.java
+++ Main.java
@@ -3,13 +3,14 @@ public class Main {
     {
         boolean[][] schedule = new boolean[8][60];
         AppointmentBook a = new AppointmentBook(schedule);
-        System.out.println(a);
         for (int i = 10; i < 15; i++) schedule [1][i] = true;
-        for (int i = 30; i < 45; i++) schedule [1][i] = true;
-        for (int i = 50; i < 60; i++) schedule [1][i] = true;
-       //a.printPeriod(2);
-        a.findFreeBlock(2, 15);
-        //System.out.println(a.findFreeBlock(2, 15));
+        for (int i = 30; i < 45; i++) schedule [2][i] = true;
+        for (int i = 50; i < 60; i++) schedule [2][i] = true;
+        for (int i = 50; i < 60; i++) schedule [3][i] = true;
+        for (int i = 44; i < 60; i++) schedule [3][i] = true;
+        //a.printPeriod(2);
+        //a.findFreeBlock(2, 13);
+        System.out.println(a.findFreeBlock(2, 15));
         
     }
 }
